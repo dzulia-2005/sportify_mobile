02fb06317cbe91dfdf4e3a210de01fe068c59c67
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-12-29 14:17:39 +0400

    feat:add createTournament Btn

--- src/screens/MySchool/TournamentTeams/components/Header.tsx
+++ src/screens/MySchool/TournamentTeams/components/Header.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { styles } from '../styles/mainStyles'
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from '../styles/mainStyles';
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   return (
-         <View style={styles.HeaderContainer}>
-            <Text style={styles.HeaderTitle}>Tournaments Teams
-            </Text>
-          </View>
-  )
-}
+    <View style={styles.HeaderContainer}>
+      <Text style={styles.HeaderTitle}>Tournaments Teams</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: '#4CAF50', padding: 10, borderRadius: 10 }}
+      >
+        <Text style={{ color: '#fff', fontSize: 14 }}>Add Tournament +</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default Header
+export default Header;
