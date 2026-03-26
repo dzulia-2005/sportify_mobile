f6e2936089c83b1c3978a2a2f53f1a339f3a5bc0
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2026-03-26 21:10:38 +0400

    feat: delete extra fields at footer

--- src/screens/dashboard/home/components/Footer.tsx
+++ src/screens/dashboard/home/components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
 import { styles } from '../styles/FooterStyles';
 import { useI18n } from '../../../../shared/lib/i18n/I18nProvider';
 
@@ -10,13 +9,6 @@ const Footer = () => {
   return (
     <View style={styles.footer}>
       <View style={styles.divider} />
-
-      <View style={styles.socials}>
-        <Icon name="github" size={22} color="#3B82F6" />
-        <Icon name="instagram" size={22} color="#3B82F6" />
-        <Icon name="mail" size={22} color="#3B82F6" />
-      </View>
-
       <Text style={styles.text}>
         © {new Date().getFullYear()} SportZone. {t('All rights reserved.')}
       </Text>
